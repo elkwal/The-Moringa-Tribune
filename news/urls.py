4bffe31104c502a6369988a3cc3c2d7ba499541c
Ekiru Timothy <ekiru@outlook.com>
2018-11-10 21:41:18 +0300

    added many things

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.news_today,name='newsToday'),
+    url(r'^search/', views.search_results, name='search_results'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 ]
